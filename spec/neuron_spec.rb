0dd985e3d84e34221ba9a94190881a323298a325
Andres Freyria <andres.freyria@gmail.com>
2011-05-21 10:30:51 -0500

    Using Let rspec syntax and adding .rvmrc file

--- spec/neuron_spec.rb
+++ spec/neuron_spec.rb
@@ -2,40 +2,38 @@ require 'spec_helper'
 
 describe Neuron do
   
-  before :each do
-    @neuron1 = Neuron.new(1)
-    @neuron2 = Neuron.new(1)
-  end
+  let(:neuron1) { Neuron.new(1) }
+  let(:neuron2) { Neuron.new(1) }
 
   it "adds an input neuron" do
-    @neuron1.add_input(@neuron2)
+    neuron1.add_input(neuron2)
 
-    @neuron1.inputs.include?(@neuron2).should be_true
-    @neuron2.outputs.include?(@neuron1).should be_true
+    neuron1.inputs.include?(neuron2).should be_true
+    neuron2.outputs.include?(neuron1).should be_true
   end
 
   it "adds an output neuron" do
-    @neuron2.add_output(@neuron1)
+    neuron2.add_output(neuron1)
 
-    @neuron1.inputs.include?(@neuron2).should be_true
-    @neuron2.outputs.include?(@neuron1).should be_true
+    neuron1.inputs.include?(neuron2).should be_true
+    neuron2.outputs.include?(neuron1).should be_true
   end
 
   it "returns the preset value (* weight) if there are no input neurons" do
-    @neuron1.value = 1
-    @neuron1.weight = 2
-    @neuron1.activation_value.should == 2
+    neuron1.value = 1
+    neuron1.weight = 2
+    neuron1.activation_value.should == 2
   end
 
   it "raises an error if there is no value to return" do
-    lambda { @neuron1.activation_value }.should raise_error
+    lambda { neuron1.activation_value }.should raise_error
   end
 
-  it "returns the sum of the inputs as the value if input neurons are present" do
-    @neuron1.add_input(@neuron2)
-    @neuron2.value = 1
+  it "returns the sum of the inputs as the activation value" do
+    neuron1.add_input(neuron2)
+    neuron2.value = 1
     
-    @neuron1.activation_value.should == 1
+    neuron1.activation_value.should == 1
   end
 
 end
